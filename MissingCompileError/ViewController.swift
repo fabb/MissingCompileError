fb3692328940aac96b1139ad1efcdf166ca51d02
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2015-05-07 17:49:56 +0200

    Initial Commit

---
+++ MissingCompileError/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MissingCompileError
+//
+//  Created by Fabian Ehrentraud on 07.05.15.
+//  Copyright (c) 2015 my.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
